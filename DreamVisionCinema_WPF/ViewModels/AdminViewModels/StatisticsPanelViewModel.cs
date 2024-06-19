2ac39781998c97909e4509c84a564ebb43bb939b
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2024-06-19 13:39:24 +0200

    Done

--- DreamVisionCinema_WPF/ViewModels/AdminViewModels/StatisticsPanelViewModel.cs
+++ DreamVisionCinema_WPF/ViewModels/AdminViewModels/StatisticsPanelViewModel.cs
@@ -18,7 +18,7 @@ namespace DreamVisionCinema_WPF.ViewModels.AdminViewModels
         public StatisticsPanelViewModel(IReservationRepository reservationRepository)
         {
             TotalIncome = (int)reservationRepository.GetTotalIncome();
-            IncomeGuideline = 10000;
+            IncomeGuideline = 15000;
             IncomePercent = CalculateIncomePercent();
             MostPopularMoviesViewCommand = new RelayCommand(OpenMostPopularMoviesView);
             MostProfitableMoviesViewCommand = new RelayCommand(OpenMostProfitableMoviesView);
